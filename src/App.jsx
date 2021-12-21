4173d45b7b6d59e0faece02a458aec97afdac1fd
oliviergfarley <olivier@sofdesk.com>
2021-12-20 19:51:18 -0500

    refreshh

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { Grid } from "@material-ui/core";
 import Link from "@material-ui/core/Link";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import logo from "./assets/hand.png";
-import dict from "./dictionnary";
-const LangueBar = ({ setLang }) => {
-  return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="flex-end"
-      alignItems="center"
-      style={{ height: "5%" }}
-    >
-      <Grid item onClick={() => setLang(0)} style={{ cursor: "pointer" }}>
-        fr
-      </Grid>
-      /
-      <Grid
-        style={{ paddingRight: "10px", cursor: "pointer" }}
-        item
-        onClick={() => setLang(1)}
-      >
-        eng
-      </Grid>
-    </Grid>
-  );
-};
 
 const LinkFooter = () => {
   return (
     <Grid
       container
-      justify="center"
+      justifyContent="center"
       alignItems="center"
       style={{ height: "10%" }}
     >
@@ -67,23 +42,19 @@ const LinkFooter = () => {
 };
 
 const App = () => {
-  const [lang, setLang] = useState(0);
-
   return (
     <div className="App">
-      <LangueBar setLang={setLang} />
       <Grid
         container
-        justify="center"
+        justifyContent="center"
         alignItems="center"
         style={{ height: "85%" }}
       >
         <Grid item xs={6} style={{ textAlign: "right" }}>
-          <div>{dict.greeting[lang] + "!"}</div>
-          <div>{dict.welcome[lang]}</div>
+          olivier
         </Grid>
-        <Grid item xs={6} style={{ textAlign: "left", paddingLeft: "10%" }}>
-          <img src={logo} alt="Logo" style={{ width: "45%" }} />
+        <Grid item xs={6} style={{ textAlign: "left", paddingLeft: "2%" }}>
+          :)
         </Grid>
       </Grid>
       <LinkFooter />
--- src/dictionnary.js
+++ src/dictionnary.js
@@ -1,6 +1,10 @@
 const dict = {
   greeting: ["salut", "hey"],
-  welcome: ["bienvenue sur notre site web", "welcome! nice to see you here"],
+  welcome: ["bienvenue sur mon site web", "welcome! nice to see you here"],
+  quickBio: [
+    "je m'appel Olivier, j'aime bien penser a comment faire les internets meilleurs",
+    "this is a bio",
+  ],
 };
 
 export default dict;
